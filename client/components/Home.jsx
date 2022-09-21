751e8564f4e8e3357cca58b292d332966aab83de
nick-echevarria <nick.echev@gmail.com>
2022-09-21 12:34:43 -0400

    feat: added in apexcharts

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import Header from '../containers/Header'; 
+import Header from '../containers/Header';
 import Sidebar from '../containers/Sidebar';
+import LineChart from './LineChart';
+import TimeSeriesChart from './TimeSeriesChart';
+import RealTimeChart from './RealTimeChart';
 
 const Home = () => {
   return (
-    <div id="home-container">
+    <div id='home-container'>
       <Header />
       <Sidebar />
-      <div id="home-main">
-      This is the home container!
+      <div id='home-main'>
+        <LineChart />
+        <TimeSeriesChart />
+        <RealTimeChart />
       </div>
     </div>
   );
-}
- 
+};
+
 export default Home;
