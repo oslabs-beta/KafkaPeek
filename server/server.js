f6fa4d0d2c114888c977194112af9654e4ca0c62
Juan Kang <juanpkang22@gmail.com>
2022-09-28 12:50:43 -0700

    minor changes

--- server/server.js
+++ server/server.js
@@ -54,12 +54,12 @@ io.on('connection', (socket) => {
       const fetchBytesIn = await fetchQuery(bytesInPerSec[0], bytesInPerSec[1]);
       const fetchBytesOut = await fetchQuery(bytesOutPerSec[0],bytesOutPerSec[1]);
       const fetchMessagesIn = await fetchQuery(messagesInPerSec[0], messagesInPerSec[1]);
-      // const fetchactiveControllerCount = await fetchQuery(activeControllerCount[0],activeControllerCount[1]);
+      const fetchactiveControllerCount = await fetchQuery(activeControllerCount[0],activeControllerCount[1]);
       socket.emit('rate',{
         bytesInPerSec: fetchBytesIn,
         bytesOutPerSec: fetchBytesOut,
         messagesInPerSec: fetchMessagesIn,
-        // activeControllerCount: fetchactiveControllerCount
+        activeControllerCount: fetchactiveControllerCount
       })
     }, 1000);
   })
