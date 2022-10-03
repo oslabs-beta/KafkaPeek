428f3752396ae954ee85e829f5a11fcae5598042
Juan Kang <juanpkang22@gmail.com>
2022-10-03 13:29:44 -0700

    added conditional for display of signin button or dashboard button

--- client/containers/Header.jsx
+++ client/containers/Header.jsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 import ZurauLogo from '../assets/ZurauLogo';
 
 const Header = () => {
+  // const [loggedIn, setLogIn] = useState(false);
+  const loggedIn = true;
+  const signinButton = <button><Link to='/signin'>Sign in {'>'}</Link></button>;
+  const dashboardButton = <button><Link to='/dashboard'>Go To Dashboard {'>'}</Link></button>;
   return (
     <div id='header-container'>
       <div id='header-left'>
@@ -11,16 +15,16 @@ const Header = () => {
       </div>
       <div id='header-mid'>
         <div id='header-mid-left'>
-          <a href=''>Documentation</a>
+          <a href='' target='_blank'>Documentation</a>
         </div>
         <div id='header-mid-right'>
-          <a href=''>Github</a>
+          <a href='https://github.com/oslabs-beta/zurau' target='_blank'>Github</a>
         </div>
       </div>
       <div id='header-right'>
-        <button>
-          <Link to='/signin'>Sign in {'>'}</Link>
-        </button>
+        {
+          !loggedIn ? signinButton : dashboardButton
+        }
       </div>
     </div>
   );
